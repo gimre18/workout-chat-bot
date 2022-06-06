c4ec635e04a6161b70fa1229ed979ebf4ce8837a
mel2047 <mel2047@EWGOWKS0266>
2022-06-06 15:26:15 +0200

    handleMessage

--- src/routes/web.js
+++ src/routes/web.js
@@ -5,7 +5,6 @@ import { postWebhook, getWebhook} from "../controllers/chatBotController.js";
 let router = express.Router();
 
 let initWebRoutes = (app) => {
-    router.get("/", getHomepage);
     router.get("/webhook", getWebhook);
     router.post("/webhook", postWebhook);
     return app.use("/", router);
